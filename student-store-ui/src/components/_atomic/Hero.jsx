682fbe0a768dc893e4e0c9edecb887f07be7ec93
Ashaun Thomas <ashaunthomas@siu.edu>
2023-05-19 17:51:20 -0700

    adds Hero

--- student-store-ui/src/components/_atomic/Hero.jsx
+++ student-store-ui/src/components/_atomic/Hero.jsx
@@ -1,8 +1,13 @@
 import * as React from "react"
+import image from '../../assets/hero.gif'
 
 
 export default function Hero() {
   return (
-    <div className="hero"></div>
+    <div className="hero">
+      <span className="intro">Welcome, shopper</span>
+      <br/>
+      <img className="hero-img" alt="logo" src={image} />
+    </div>
   )
 }
