49914573db386c8b88a7bfe1095904a3d11a7ce9
Ashaun Thomas <ashaunthomas@siu.edu>
2023-05-19 20:42:39 -0700

    adds sidebar

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,44 @@
 import * as React from "react"
+import { useState } from 'react';
 import "./Sidebar.css"
+import PropTypes from 'prop-types';
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+  [isOpen, setIsOpen] = useState(props.isOpen);
+
+  function handleOnToggle() {
+    setIsOpen(!isOpen)
+  }
+
+  function handleAdditionalDetails() {
+    if (isOpen) {
+      return (
+        <>
+          <ShoppingCart />
+          <CheckoutForm />
+        </>
+      )
+    } else {
+      return null
+    }
+  }
+
   return (
-    <section className="sidebar">
-      <p>Sidebar</p>
+    <section className="sidebar" style={{ width: isOpen ? '351px' : '150px' }}>
+      <button className="toggle-button" onClick={handleOnToggle}>Toggle</button>
+      { handleAdditionalDetails() }
     </section>
   )
+}
+
+Sidebar.propTypes = {
+  isOpen: PropTypes.bool,
+  shoppingCart: PropTypes.shape({
+    itemId: PropTypes.any,
+    quantity: PropTypes.number
+  }),
+  products: PropTypes.any,
+  checkoutForm: PropTypes.any,
+  handleOnCheckoutFormChange: PropTypes.func,
+  handleOnSubmitCheckoutForm: PropTypes.func,
 }
